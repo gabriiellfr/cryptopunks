58d96687f9cca0e75d7d0912f616d0267f5358c3
GabrielSantos <gabriiellfr@hotmail.com>
2023-06-04 01:51:59 -0300

    Add Heads Main Fn

---
+++ heads/main.py
@@ -0,0 +1,21 @@
+import heads.male as male
+import heads.female as female
+
+import utils.choose_attribute as choose_attribute
+
+heads = {
+    "Male": male,
+    "Female": female,
+}
+
+head_ratio = {
+    "Male": 0.5,  # 5000 in 10,000
+    "Female": 0.5,  # 5000 in 10,000
+}
+
+def draw_head(draw):
+    chosen_head = choose_attribute.main(heads, head_ratio)
+
+    head_type, head_outline_pixels = heads[chosen_head].draw(draw)
+
+    return chosen_head, head_type, head_outline_pixels
